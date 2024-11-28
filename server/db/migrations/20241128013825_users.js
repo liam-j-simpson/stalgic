d8f014ca63277fdd1d973a1d31d80c07de5e45ff
Abdullah Daulatzai <abdullahdaulatzai1@gmail.com>
2024-11-28 17:26:22 +1300

    fix conflict

--- server/db/migrations/20241128013825_users.js
+++ server/db/migrations/20241128013825_users.js
@@ -2,9 +2,14 @@ export function up(knex) {
   return knex.schema.createTable('users', (table) => {
     table.string('auth0_id').primary()
     table.string('name').notNullable()
+<<<<<<< HEAD
     table.string('email').notNullable()
     table.string('dob').notNullable()
     table.string('profile_image')
+=======
+    table.string('email').notNullable().unique()
+    table.string('dob').notNullable()
+>>>>>>> beb55e6 (add user table with columns)
   })
 }
 
