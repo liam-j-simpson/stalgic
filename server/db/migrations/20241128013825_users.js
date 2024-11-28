136b991528433e7db773a10729c0982213f5d992
Abdullah Daulatzai <abdullahdaulatzai1@gmail.com>
2024-11-28 14:44:42 +1300

    add user table with columns

---
+++ server/db/migrations/20241128013825_users.js
@@ -0,0 +1,12 @@
+export function up(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.string('auth0_id').primary()
+    table.string('name').notNullable()
+    table.string('email').notNullable().unique()
+    table.string('dob').notNullable()
+  })
+}
+
+export function down(knex) {
+  return knex.schema.dropTable('users')
+}
