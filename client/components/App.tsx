f87bff89b7a7a5570a94b23ca0929d96bad3f284
Nikki Cupples <ncupples03@gmail.com>
2024-11-28 22:35:04 +1300

    add styling to navigation bar

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,11 +1,9 @@
-import { Button } from '../ui/Button.tsx'
 import Nav from './Navigation/Nav'
 
 function App() {
   return (
     <>
       <div className="app">
-        <Button>Hi</Button>
         <Nav />
       </div>
     </>
