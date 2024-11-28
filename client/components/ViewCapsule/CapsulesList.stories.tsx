ee327ab07ffaa4319bd4e47654d3020f82e4eaa5
Nikki <178754087+nikki-cupples@users.noreply.github.com>
2024-11-28 17:35:19 +1300

    add working on storybook

--- client/components/ViewCapsule/CapsulesList.stories.tsx
+++ client/components/ViewCapsule/CapsulesList.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react"
+import type { Meta, StoryObj } from '@storybook/react-vite'
 
 // import ListAllCapsules from './ListAllCapsules'
 import CapsuleListItem from './CapsuleListItem'
