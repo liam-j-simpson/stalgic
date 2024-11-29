1cae9288635c2d3d10d3e902686f05cccdb1d26a
Liam <175756297+liam-j-simpson@users.noreply.github.com>
2024-11-29 15:40:40 +1300

    creating the look of the page

--- client/Pages/AddCapsulePage.tsx
+++ client/Pages/AddCapsulePage.tsx
@@ -3,29 +3,32 @@ import AddCapsule from '../components/AddCapsule/AddCapsule'
 function AddCapsulePage() {
   //include all the hooks and smart data that gets passed to the apis
   const [description, setDescription] = useState('')
-  const [tags, setTags] = useState('')
+  const [tags, setTags] = useState([''])
   const [title, setTitle] = useState('')
   const [date, setDate] = useState<Date>()
 
   console.log('description', description)
   console.log('tags', tags)
   console.log('title', title)
-  console.log('date', date)
+  // console.log('date', typeof )
 
   return (
     <body>
-      <h1 className="font-lalezar text-9xl">Create a Capsule</h1>
       <section className="flex">
-        <div className="w-96 flex-col ">
+        <div className="m-16">
+          <h1 className="font-lalezar text-9xl text-white">CREATE</h1>
+          <h2 className="font-lalezar mb-12 text-7xl text-white">{title.length <= 0 ? 'CAPSULE' : `${title.toUpperCase()} CAPSULE`}</h2>
           <AddCapsule
             setTitle={setTitle}
+            title={title}
+            description={description}
             setDescription={setDescription}
+            tags={tags}
             setTags={setTags}
-            setDate={setDate}
             date={date}
+            setDate={setDate}
           />
         </div>
-        <div className="flex-col ">Title</div>
       </section>
     </body>
   )
--- client/components/AddCapsule/AddCapsule.tsx
+++ client/components/AddCapsule/AddCapsule.tsx
@@ -9,8 +9,26 @@ import { Input } from '../../ui/Input'
 import { Label } from '../../ui/Label'
 import { useAddCapsule } from '../../hooks/useAddCapsule'
 import { Accordion } from '../../ui/Accordian'
-
-function AddCapsule({ setTitle, setDescription, setTags, setDate, date }) {
+interface Props {
+  setTitle: React.Dispatch<React.SetStateAction<string>>
+  setDescription: React.Dispatch<React.SetStateAction<string>>
+  setTags: React.Dispatch<React.SetStateAction<string[]>>
+  setDate: React.Dispatch<React.SetStateAction<Date | undefined>>
+  date: Date | undefined
+  title: string
+  tags: string[]
+  description: string
+}
+function AddCapsule({
+  setTitle,
+  title,
+  setDescription,
+  description,
+  setTags,
+  tags,
+  setDate,
+  date,
+}: Props) {
   //multi input function
 
   // const handleAddCapsule = useAddCapsule(a)
@@ -38,30 +56,60 @@ function AddCapsule({ setTitle, setDescription, setTags, setDate, date }) {
 
   return (
     <>
-      <main>
-        <form></form>
-        <Label htmlFor="title">Title</Label>
-        <Input
-          id="title"
-          onChange={handleTitleChange}
-          placeholder="Enter title"
-        />
-        <br />
-        <Label>
-          Opening Date
-          <DatePicker setDate={setDate} date={date}></DatePicker>
-        </Label>
-        <br />
-        <Label htmlFor="description">Description</Label>
-        <Input
-          onChange={handleDescriptionChange}
-          id="description"
-          placeholder="Enter description"
-        />
-        <Label htmlFor="tags">Tags</Label>
-        <Input onChange={handleTagsChange} id="tags" placeholder="Enter tags" />
+      <main className="flex">
+        <div
+          className={`mb-96 mr-12 flex h-96 w-80 flex-col rounded-lg bg-[#ffffff] p-6 text-[#13A25B]`}
+        >
+          <form>
+            <Label htmlFor="title">Title</Label>
+            <Input
+              id="title"
+              onChange={handleTitleChange}
+              placeholder="Enter title"
+            />
+            <br />
+            <Label>
+              Opening Date
+              <DatePicker setDate={setDate} date={date}></DatePicker>
+            </Label>
+            <br />
+            <Label htmlFor="description">Description</Label>
+            <Input
+              onChange={handleDescriptionChange}
+              id="description"
+              placeholder="Enter description"
+            />
+            <Label htmlFor="tags">Tags</Label>
+            <Input
+              onChange={handleTagsChange}
+              id="tags"
+              placeholder="Enter tags"
+            />
 
-        <Button>Submit</Button>
+            {/* <Button onSubmit={handleSubmit}>Submit</Button> */}
+          </form>
+        </div>
+        <div className="w-1/2">
+          <div
+            className={`mb-96 mr-12 flex h-96 w-80 flex-col rounded-lg bg-[#ffffff] p-6 text-[#13A25B]`}
+          >
+            <h1 className="font-lalezar text-5xl">
+              {date === undefined ? 'Time Remaining' : date?.toLocaleString()}
+            </h1>
+
+            <p className="font-labrada space-l text-xl">
+              {title.length <= 0 ? 'Title' : title}
+            </p>
+
+            <p className="font-labrada space-l text-xl">
+              {description.length <= 0 ? 'Description' : description}
+            </p>
+
+            <p className="font-labrada space-l text-xl">
+              {tags.length <= 1 ? 'Tags' : tags}
+            </p>
+          </div>
+        </div>
       </main>
     </>
   )
