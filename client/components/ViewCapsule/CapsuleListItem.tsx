273c76459211e23544b02f800c0ac490fa72e5f5
Nikki <178754087+nikki-cupples@users.noreply.github.com>
2024-11-29 16:28:16 +1300

    add working time function

--- client/components/ViewCapsule/CapsuleListItem.tsx
+++ client/components/ViewCapsule/CapsuleListItem.tsx
@@ -1,7 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 import { Capsule } from '../../../models/capsule'
+import TimeFunction from '../Time/TimeFunction'
 interface Props {
   capsule: Capsule
 }
@@ -36,46 +37,7 @@ function CapsuleListItem(props: Props) {
     })
   }
 
-  const [timeRemaining, setTimeRemaining] = useState<{
-    years: number
-    days: number
-    hours: number
-    minutes: number
-    seconds: number
-  } | null>(null)
-
-  const parseTime = (time: string): Date => {
-    const [day, month, yearAndTime] = time.split('/')
-    const [year, timePart] = yearAndTime.split(' ')
-    const dateString = `${year}-${month}-${day}T${timePart}:00`
-    return new Date(dateString)
-  }
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const now = new Date()
-      const openingTime = parseTime(capsule.time)
-      const timeDiff = openingTime.getTime() - now.getTime()
-
-      if (timeDiff <= 0) {
-        setTimeRemaining(null)
-        clearInterval(interval)
-      } else {
-        const totalSeconds = Math.floor(timeDiff / 1000)
-        const years = Math.floor(totalSeconds / (60 * 60 * 24 * 365))
-        const days = Math.floor(
-          (totalSeconds % (60 * 60 * 24 * 365)) / (60 * 60 * 24),
-        )
-        const hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60))
-        const minutes = Math.floor((totalSeconds % (60 * 60)) / 60)
-        const seconds = totalSeconds % 60
-
-        setTimeRemaining({ years, days, hours, minutes, seconds })
-      }
-    }, 1000)
-
-    return () => clearInterval(interval)
-  }, [capsule.time])
+  const time = TimeFunction(capsule.time)
 
   return (
     <>
@@ -84,29 +46,8 @@ function CapsuleListItem(props: Props) {
         onMouseLeave={handleNoHover}
         className={`mb-96 mr-12 flex h-96 w-80 flex-col rounded-lg bg-[#ffffff] p-6 text-[#13A25B] ${hover.divBg} ${hover.divText}`}
       >
-        <div className="font-bold ">
-          {timeRemaining ? (
-            <div className="">
-              <div className="text-[48px]">
-                {timeRemaining.years > 0 && (
-                  <span>{timeRemaining.years}y </span>
-                )}
-                {timeRemaining.days > 0 && <span>{timeRemaining.days}d </span>}
-              </div>
-              <div className="text-[22px]">
-                {timeRemaining.hours > 0 && (
-                  <span>{timeRemaining.hours}h </span>
-                )}
-                {timeRemaining.minutes > 0 && (
-                  <span>{timeRemaining.minutes}m </span>
-                )}
-                <span>{timeRemaining.seconds}s</span>
-              </div>
-            </div>
-          ) : (
-            <p className="text-[24px] text-[#FE5801]">Opened!</p>
-          )}
-
+        <div className="font-bold">
+          {time}
           <div className="pb-4">
             <h2 className="pb-2 pt-4 font-bold">{capsule.title}</h2>
             <p>{capsule.description}</p>
