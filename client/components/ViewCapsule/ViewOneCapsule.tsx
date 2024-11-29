4ee3cd308408cb93ac680fddc7dec1476e70d165
Nikki <178754087+nikki-cupples@users.noreply.github.com>
2024-11-29 13:09:56 +1300

    add proper styling for title

--- client/components/ViewCapsule/ViewOneCapsule.tsx
+++ client/components/ViewCapsule/ViewOneCapsule.tsx
@@ -1,9 +1,13 @@
 function ViewOneCapsule() {
   return (
     <>
-    <div>
-    <h1>I am a capsule</h1>
-    </div>
+      <section className="bg-[#13A25B] pl-16 font-lalezar">
+        <div className="flex flex-row">
+          <h1 className="be-cover pt-20 font-lalezar text-9xl font-bold tracking-wider text-white">
+            UPLOAD
+          </h1>
+        </div>
+      </section>
     </>
   )
 }
