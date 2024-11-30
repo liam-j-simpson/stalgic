4a6746a6a32c0fb7e3af6a70c4655626b1fa066b
Nikki Cupples <ncupples03@gmail.com>
2024-11-30 23:05:35 +1300

    add test for get capsule by id

---
+++ client/components/ViewCapsule/ViewOneCapsule.test.tsx
@@ -0,0 +1,43 @@
+import { describe, it, expect, vi, afterEach } from 'vitest'
+import nock from 'nock'
+import { getCapsuleById } from '../../apis/api'
+import { useAuth0 } from '@auth0/auth0-react'
+
+vi.mock('@auth0/auth0-react', () => ({
+  useAuth0: () => ({
+    user: {
+      sub: 'auth0|123',
+      email: '',
+    },
+    isAuthenticated: true,
+    getAccessTokenSilently: vi.fn(() => 'token'),
+  }),
+}))
+
+describe('List all capsules', () => {
+  afterEach(() => {
+    nock.cleanAll()
+  })
+  
+  it('GET BY ID route should return one capsule', async () => {
+    const mockCapsuleData = {
+      title: 'Guasha Progress',
+      time: '30/11/2026 14:00',
+      description: 'Contour that face',
+      tags: ['health', 'wellbeing'],
+      id: 1,
+    }
+
+    const getScope = nock('http://localhost')
+      .get('/api/v1/capsule/1')
+      .reply(200, mockCapsuleData)
+
+    const { getAccessTokenSilently } = useAuth0()
+    const token = await getAccessTokenSilently()
+
+    const result = await getCapsuleById(token, 1)
+
+    expect(result).toEqual(mockCapsuleData)
+    expect(getScope.isDone()).toBeTruthy()
+  })
+})
