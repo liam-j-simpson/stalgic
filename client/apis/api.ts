454cfe61bbfe30fb421a8cefdda66ebfce36c665
Nikki Cupples <ncupples03@gmail.com>
2024-11-30 18:35:18 +1300

    fixing formatting of files

--- client/apis/api.ts
+++ client/apis/api.ts
@@ -1,9 +1,6 @@
-// General API folder
-// Will have individual hooks for each route
-// Import all of our hooks into here
-
 import request from 'superagent'
 import { CapsuleData } from '../../models/capsule'
+import { User } from '../../models/user'
 
 // -- GET ALL CAPSULES -- //
 export async function getCapsules(token: string) {
@@ -23,4 +20,21 @@ export async function getCapsuleById(token: string, id: number) {
     .set('Content-Type', 'application/json')
 
   return res.body as CapsuleData
+}
+
+// -- GET USER DATA BY AUTH0-ID -- //
+export async function getUser(auth0_id: string, token: string) {
+  const res = await request
+    .get(`/api/v1/user/${auth0_id}`)
+    .set('Authorization', `Bearer ${token}`);
+  return res.body;
+}
+
+// -- UPSERT USER PROFILE (ADD OR UPDATE) -- //
+export async function upsertUser(profileData: User, token: string) {
+  const res = await request
+    .post('/api/v1/user/')
+    .set('Authorization', `Bearer ${token}`)
+    .send(profileData)
+  return res.body
 }
