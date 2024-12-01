e7e4d3863d48db1e3993921e1457de1afc0435ec
Nikki Cupples <ncupples03@gmail.com>
2024-12-01 18:23:38 +1300

    add initial layout for landing page

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom'
+import LandingPage from '../Pages/LandingPage'
 
 function App() {
   return (
     <>
       <div className="app">
+        <LandingPage />
         <Outlet />
       </div>
     </>
