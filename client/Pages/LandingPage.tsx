3eddd744f8708481553804d68c336f55d221b746
Liam <175756297+liam-j-simpson@users.noreply.github.com>
2024-12-02 10:59:56 +1300

    adding landing page sign in button and adding links from nav to profile page

--- client/Pages/LandingPage.tsx
+++ client/Pages/LandingPage.tsx
@@ -1,12 +1,51 @@
+import { useEffect } from 'react'
+import { IfNotAuthenticated } from '../components/Authentication/Authenticated'
+import { useAuth0 } from '@auth0/auth0-react'
+import { useUpsertProfile } from '../hooks/useEditProfile'
+import { User } from '../../models/user'
+
 function LandingPage() {
-  // TODO: Ask the team whether we are happy with the simplicity, or if we want to have something a bit more complicated?
+  const { user, loginWithRedirect } = useAuth0()
+  const { handleProfileUpsert } = useUpsertProfile()
+
+  const handleLogIn = async () => {
+    try {
+      await loginWithRedirect()
+    } catch (error) {
+      console.error('Error during login:', error)
+    }
+  }
+
+  useEffect(() => {
+    if (user) {
+      const profileData: User = {
+        auth0_id: user?.sub || '',
+        name: user?.name || '',
+        email: user?.email || '',
+        dob: user?.birthdate || null,
+        profile_image: user?.picture || null,
+      }
+      handleProfileUpsert(profileData)
+    }
+  }, [user, handleProfileUpsert])
+
   return (
     <>
       <section className="bg-[#13A25B] pl-16 font-lalezar">
         <div className="flex flex-col">
-          <h1 className="be-cover pt-20 mt-32 font-lalezar text-9xl font-bold tracking-wider text-white">
+          <h1 className="be-cover mt-32 pt-20 font-lalezar text-9xl font-bold tracking-wider text-white">
             nostalgia grown with time
           </h1>
+          <IfNotAuthenticated>
+            <div className="flex items-center hover:underline ">
+              <button
+                onClick={handleLogIn}
+                className="mr-28 inline-block h-14 w-36 rounded-full bg-[#ffffff] text-2xl font-bold text-[#13A25B] hover:bg-[#FE5801] hover:text-[#ffffff]"
+              >
+                <p className="text-l space-l font-labrada">Sign in</p>
+              </button>
+            </div>
+          </IfNotAuthenticated>
         </div>
       </section>
     </>
