303f5a7a9a9f451d29abbb78764480c4959ccbdf
Abdullah Daulatzai <abdullahdaulatzai1@gmail.com>
2024-12-02 14:59:57 +1300

    fix runtime error

--- server/db/connection.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var knex_1 = require("knex");
-var knexfile_1 = require("./knexfile");
-var env = process.env.NODE_ENV || 'development';
-var connection = (0, knex_1.default)(knexfile_1.default[env]);
-exports.default = connection;
