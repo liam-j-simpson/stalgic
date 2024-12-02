1a06cbca1e5165a1b282dca6eba2562d1cddbb65
Liam <175756297+liam-j-simpson@users.noreply.github.com>
2024-12-02 19:19:57 +1300

    form aesthetically complete, needs hook and db function

--- client/Pages/ProfilePage.tsx
+++ client/Pages/ProfilePage.tsx
@@ -8,11 +8,11 @@ import DatePicker from 'react-datepicker'
 import { format } from 'date-fns'
 
 function ProfilePage() {
-  // const updateProfile =
+  // const updateProfileMutation = useUpdateProfile()
   // const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
   //   e.preventDefault()
 
-  //   updateProfile.mutate({
+  //   updateProfileMutation.mutate({
   //     name: profileForm.name,
   //     email: profileForm.email,
   //     dob: profileForm.dob,
@@ -89,88 +89,88 @@ function ProfilePage() {
         </div>
 
         <section className="bg-[#13A25B] bg-[#13A25B] pb-12  pl-16 font-lalezar">
-          {/* <form>  */}
-          <div className="flex">
-            <div
-              className={`mb-8 mr-12 h-64 w-80 rounded-lg bg-[#ffffff] p-6 text-[#13A25B]`}
-            >
-              <h2 className="mb-12 font-lalezar text-7xl text-[#13A25B]">
-                NAME
-              </h2>
-              <p className="space-l mb-4 font-labrada text-xl">
-                {data.name ? data.name : 'name not found'}
-              </p>
-              {edit === true && (
-                <Input
-                  aria-label="name"
-                  type="name"
-                  id="name"
-                  name="name"
-                  onChange={handleChange}
-                  placeholder="Edit name"
-                  value={profileForm.name}
-                />
-              )}
+          <form>
+            <div className="flex">
+              <div
+                className={`mb-8 mr-12 h-64 w-80 rounded-lg bg-[#ffffff] p-6 text-[#13A25B]`}
+              >
+                <h2 className="mb-12 font-lalezar text-7xl text-[#13A25B]">
+                  NAME
+                </h2>
+                <p className="space-l mb-4 font-labrada text-xl">
+                  {data.name ? data.name : 'name not found'}
+                </p>
+                {edit === true && (
+                  <Input
+                    aria-label="name"
+                    type="name"
+                    id="name"
+                    name="name"
+                    onChange={handleChange}
+                    placeholder="Edit name"
+                    value={profileForm.name}
+                  />
+                )}
+              </div>
+              <div
+                className={`mb-8 mr-12  h-64 w-80 rounded-lg bg-[#ffffff] p-6 text-[#13A25B]`}
+              >
+                <h2 className="mb-12 font-lalezar text-7xl text-[#13A25B]">
+                  EMAIL
+                </h2>
+                <p className="space-l mb-4 font-labrada text-xl">
+                  {data.email ? data.email : 'email not found'}
+                </p>
+                {edit === true && (
+                  <Input
+                    aria-label="email"
+                    type="email"
+                    id="email"
+                    name="email"
+                    onChange={handleChange}
+                    placeholder="edit email"
+                    value={profileForm.email}
+                  />
+                )}
+              </div>
+              <div
+                className={`mb-8 mr-12  h-64 w-80 rounded-lg bg-[#ffffff] p-6 text-[#13A25B]`}
+              >
+                <h2 className="mb-12 font-lalezar text-7xl text-[#13A25B]">
+                  DOB
+                </h2>
+                <p className="space-l mb-4 font-labrada text-xl">
+                  {data.dob ? data.dob : 'DOB not found'}
+                </p>
+                {edit === true && (
+                  <DatePicker
+                    className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+                    selected={date}
+                    onChange={handleDateChange}
+                  />
+                )}
+              </div>
             </div>
-            <div
-              className={`mb-8 mr-12  h-64 w-80 rounded-lg bg-[#ffffff] p-6 text-[#13A25B]`}
-            >
-              <h2 className="mb-12 font-lalezar text-7xl text-[#13A25B]">
-                EMAIL
-              </h2>
-              <p className="space-l mb-4 font-labrada text-xl">
-                {data.email ? data.email : 'email not found'}
-              </p>
-              {edit === true && (
-                <Input
-                  aria-label="email"
-                  type="email"
-                  id="email"
-                  name="email"
-                  onChange={handleChange}
-                  placeholder="edit email"
-                  value={profileForm.email}
-                />
-              )}
-            </div>
-            <div
-              className={`mb-8 mr-12  h-64 w-80 rounded-lg bg-[#ffffff] p-6 text-[#13A25B]`}
-            >
-              <h2 className="mb-12 font-lalezar text-7xl text-[#13A25B]">
-                DOB
-              </h2>
-              <p className="space-l mb-4 font-labrada text-xl">
-                {data.dob ? data.dob : 'DOB not found'}
-              </p>
-              {edit === true && (
-                <DatePicker
-                  className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
-                  selected={date}
-                  onChange={handleDateChange}
-                />
-              )}
-            </div>
-          </div>
-          {edit === false && (
-            <button
-              onClick={() => setEdit(!edit)}
-              className="rounded-full bg-[#ffffff] px-8 py-2 text-3xl text-[#13A25B]"
-            >
-              edit
-            </button>
-          )}
-          {edit === true && (
-            <>
+            {edit === false && (
               <button
                 onClick={() => setEdit(!edit)}
                 className="rounded-full bg-[#ffffff] px-8 py-2 text-3xl text-[#13A25B]"
               >
-                {' '}
-                Save
+                edit
               </button>
-            </>
-          )}
-          {/* </form>*/}
+            )}
+            {edit === true && (
+              <>
+                <button
+                  onClick={() => setEdit(!edit)}
+                  className="rounded-full bg-[#ffffff] px-8 py-2 text-3xl text-[#13A25B]"
+                >
+                  {' '}
+                  Save
+                </button>
+              </>
+            )}
+          </form>
         </section>
       </>
     )
