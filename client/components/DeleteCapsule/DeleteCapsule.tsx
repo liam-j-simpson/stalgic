fd3b4aa94f0d33f91b7b812a4cbe47f47fd15fa1
Nikki Cupples <ncupples03@gmail.com>
2024-12-02 19:44:42 +1300

    add hooks for edit or delete capsule

--- client/components/DeleteCapsule/DeleteCapsule.tsx
+++ client/components/DeleteCapsule/DeleteCapsule.tsx
@@ -1,3 +1,31 @@
-// Link to hook
-// Smart/Dumb Component
-// Reference rcmndr for layout tips
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import * as api from '../../apis/api.ts'
+import { useAuth0 } from '@auth0/auth0-react'
+import { useNavigate } from 'react-router-dom'
+
+function DeleteCapsule() {
+  const queryClient = useQueryClient()
+  const { user, getAccessTokenSilently } = useAuth0()
+  const navigate = useNavigate()
+
+  return useMutation({
+    mutationFn: async (id: number) => {
+      if (!user || !user.sub) {
+        throw new Error("User is not authenticated or missing 'sub' property.")
+      }
+
+      const accessToken = await getAccessTokenSilently()
+
+      await api.deleteCapsule(accessToken, id)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['capsules'] })
+      navigate('/dashboard')
+    },
+    onError: (error) => {
+      console.error('Error during deletion:', error)
+    },
+  })
+}
+
+export default DeleteCapsule
--- client/components/EditCapsule/EditCapsule.tsx
+++ client/components/EditCapsule/EditCapsule.tsx
@@ -1,7 +1,27 @@
-import * as api from '../apis/api.ts'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import * as api from '../../apis/api.ts'
+import { useAuth0 } from '@auth0/auth0-react'
+import { CapsuleData } from '../../../models/capsule.ts'
 
-function EditCapsule() {
-  return
+function EditCapsule(id: number) {
+  const queryClient = useQueryClient()
+  const { user, getAccessTokenSilently } = useAuth0()
+
+  return useMutation({
+    mutationFn: async (capsuleData: CapsuleData) => {
+      if (!user || !user.sub) {
+        throw new Error("User is not authenticated or missing 'sub' property.")
+      }
+      const accessToken = await getAccessTokenSilently()
+      await api.editCapsuleInformation(accessToken, capsuleData)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['capsule', id] })
+    },
+    onError: (error) => {
+      console.error('Error during mutation:', error)
+    },
+  })
 }
 
 export default EditCapsule
