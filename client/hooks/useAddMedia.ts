f5e63b5768a88e8806a68047e46075293e4c6491
Nikki <178754087+nikki-cupples@users.noreply.github.com>
2024-12-02 14:44:11 +1300

    add hook for useAddMedia

--- client/hooks/useAddMedia.ts
+++ client/hooks/useAddMedia.ts
@@ -1,3 +1,32 @@
-// Need to figure out how to add media to capsules
-// By capsule id
-// Link to backend multer 
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import * as api from '../apis/api.ts'
+import { MediaData } from '../../models/media.ts'
+import { useAuth0 } from '@auth0/auth0-react'
+
+export function useAddMedia() {
+  const client = useQueryClient()
+
+  const { user, getAccessTokenSilently } = useAuth0()
+
+  return useMutation({
+    mutationFn: async ({ file }: { file: MediaData }) => {
+      if (!user || !user.sub) {
+        throw new Error("User is not authenticated or missing 'sub' property.")
+      }
+
+      const token = await getAccessTokenSilently()
+
+      const formData = new FormData()
+      formData.append('image_url', file.image_url)
+
+      await api.addMedia(file, token)
+    },
+
+    onSuccess: () => {
+      client.invalidateQueries({ queryKey: ['media'] })
+    },
+    onError: (error) => {
+      console.error('Error during mutation:', error)
+    },
+  })
+}
